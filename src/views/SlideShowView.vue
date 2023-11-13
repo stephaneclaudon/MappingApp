06556c73e522ea0ef7b292ef519ceebffe360fda
ValentinGassend <valentin.gassend@gmail.com>
2023-11-13 13:38:32 +0100

    Update SlideShowView.vue

--- src/views/SlideShowView.vue
+++ src/views/SlideShowView.vue
@@ -1,24 +1,19 @@
-< script setup >
-  import VideoViewer from '@/components/VideoViewer.vue' <
-  /script>
+<script setup>
+import VideoViewer from '@/components/VideoViewer.vue'
+</script>
 
-  <
-  template >
-  <
-  div class = "slideshow" >
-  <
-  VideoViewer / >
-  <
-  /div> <
-  /template>
+<template>
+  <div class="slideshow">
+    <VideoViewer/>
+  </div>
+</template>
 
-  <
-  style >
-  @media(min - width: 1024 px) {
-    .slideshow {
-      min - height: 100 vh;
-      display: flex;
-      align - items: center;
-    }
-  } <
-  /style>
+<style>
+@media (min-width: 1024px) {
+  .slideshow {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
