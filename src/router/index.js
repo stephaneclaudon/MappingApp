5e8c49ce0e1e9f3dcaa0927629d8e0cdad46b32f
ValentinGassend <valentin.gassend@gmail.com>
2023-11-14 11:03:21 +0100

    margin withbaord and sitcker on same vue

--- src/router/index.js
+++ src/router/index.js
@@ -21,14 +21,6 @@ const router = createRouter({
       component: () => import('../views/SlideShowView.vue')
     },
     {
-      path: '/stickers',
-      name: 'stickers',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/StickersView.vue')
-    },
-    {
       path: '/whiteboard',
       name: 'whiteboard',
       // route level code-splitting
