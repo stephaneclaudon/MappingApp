81fd1cf711e362803ac9a39b46aefddb441b801f
zaynbsn <nils.vainui@gmail.com>
2023-11-14 09:19:51 +0100

    whiteboard style & add pen sizes choice

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -3,15 +3,20 @@
 <template>
     <div class="whiteboard">
       <!-- <h1 style="color: white">{{ message }}</h1> -->
-      <canvas ref="canvas"></canvas>
       <div class="tools">
-        <button v-for="(color, index) in colors" :key="index" @click="changeColor(color)" class="color-box" :style="{backgroundColor: color}"></button>
+        <div class="size-boxes-container">
+          <button v-for="(size, index) in sizes" :key="index" @click="changeSize(size)" class="box" :style="getPenSize(size)"></button>
+        </div>
+        <div class="size-boxes-container">
+          <button v-for="(color, index) in colors" :key="index" @click="changeColor(color)" class="box" :style="getPenColor(color)"></button>
+        </div>
         <button @click="clearCanvas" class="clear">Clear</button>
       </div>
+      <canvas ref="canvas"></canvas>
     </div>
   </template>
   
-  <script>
+<script>
   import "../assets/scss/pages/_whiteboard.scss"
   export default {
     data() {
@@ -21,58 +26,68 @@
         canvas: null,
         ctx: null,
         colors: ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"],
+        sizes: [12, 16, 20, 24, 28],
         deviceType: null,
       };
     },
     mounted() {
-      this.canvas = this.$refs.canvas;
-      this.ctx = this.canvas.getContext("2d");
+      this.canvas = this.$refs.canvas
+      this.ctx = this.canvas.getContext("2d")
 
       // Set default stroke color
-      this.ctx.strokeStyle = this.colors[0];
+      this.ctx.strokeStyle = this.colors[0]
 
       // Resize canvas
-      this.canvas.height = window.innerHeight * 0.9;
-      this.canvas.width = window.innerWidth * 0.8;
+      this.canvas.height = window.innerHeight * 0.9
+      this.canvas.width = window.innerWidth * 0.8
 
-      this.setDeviceType();
+      this.setDeviceType()
       // console.log(this.deviceType)
-      this.setupEventListeners();
+      this.setupEventListeners()
     },
     methods: {
       setDeviceType() {
-        const platform = navigator.userAgentData.platform.toLowerCase();
+        const platform = navigator.userAgentData.platform.toLowerCase()
         if (/(android|webos|iphone|ipad|ipod|blackberry|windows phone)/.test(platform)) {
-          this.deviceType = 'mobile';
+          this.deviceType = 'mobile'
         } else if (/mac|win|linux/i.test(platform)) {
-          this.deviceType = 'desktop';
+          this.deviceType = 'desktop'
         } else if (/tablet|ipad/i.test(platform)) {
-          this.deviceType = 'tablet';
+          this.deviceType = 'tablet'
         } else {
-          this.deviceType = 'unknown';
+          this.deviceType = 'unknown'
         }
       },
       setupEventListeners() {
         // For desktop mouse
         if (this.deviceType === 'desktop') {
-          this.canvas.addEventListener("mousedown", this.startPainting);
-          this.canvas.addEventListener("mouseup", this.finishedPainting);
-          this.canvas.addEventListener("mousemove", this.draw);
+          this.canvas.addEventListener("mousedown", this.startPainting)
+          this.canvas.addEventListener("mouseup", this.finishedPainting)
+          this.canvas.addEventListener("mousemove", this.draw)
         } else if (this.deviceType === 'mobile') {
           // For mobile touch
-          this.canvas.addEventListener("touchstart", this.startPainting);
-          this.canvas.addEventListener("touchend", this.finishedPainting);
-          this.canvas.addEventListener("touchmove", this.mobileDraw);
+          this.canvas.addEventListener("touchstart", this.startPainting)
+          this.canvas.addEventListener("touchend", this.finishedPainting)
+          this.canvas.addEventListener("touchmove", this.mobileDraw)
         }
       },
       changeColor(color) {
-        this.ctx.strokeStyle = color;
+        this.ctx.strokeStyle = color
+      },
+      getPenColor(color) {
+        return {backgroundColor: color}
+      },
+      changeSize(size) {
+        this.ctx.lineWidth = size
+      },
+      getPenSize(size) {
+        return {width: size + 'px!important', height: size + 'px!important'}
       },
       clearCanvas() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
       },
       startPainting(e) {
-        this.painting = true;
+        this.painting = true
         if (this.deviceType === 'desktop') {
           this.draw(e);
         } else if (this.deviceType === 'mobile') {
@@ -80,49 +95,33 @@
         }
       },
       finishedPainting() {
-        this.painting = false;
-        this.ctx.beginPath();
+        this.painting = false
+        this.ctx.beginPath()
       },
       draw(e) {
-        if (!this.painting) return;
+        if (!this.painting) return
 
-        this.ctx.lineWidth = 10;
-        this.ctx.lineCap = "round";
+        // this.ctx.lineWidth = 10;
+        this.ctx.lineCap = "round"
 
-        this.ctx.lineTo(e.clientX - this.canvas.offsetLeft, e.clientY - this.canvas.offsetTop);
-        this.ctx.stroke();
+        this.ctx.lineTo(e.clientX - this.canvas.offsetLeft, e.clientY - this.canvas.offsetTop)
+        this.ctx.stroke()
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(e.clientX - this.canvas.offsetLeft, e.clientY - this.canvas.offsetTop);
+        this.ctx.beginPath()
+        this.ctx.moveTo(e.clientX - this.canvas.offsetLeft, e.clientY - this.canvas.offsetTop)
       },
       mobileDraw(e) {
-        if (!this.painting) return;
+        if (!this.painting) return
 
         this.ctx.lineWidth = 10;
-        this.ctx.lineCap = "round";
+        this.ctx.lineCap = "round"
 
-        this.ctx.lineTo(e.touches[0].clientX - this.canvas.offsetLeft, e.touches[0].clientY - this.canvas.offsetTop);
-        this.ctx.stroke();
+        this.ctx.lineTo(e.touches[0].clientX - this.canvas.offsetLeft, e.touches[0].clientY - this.canvas.offsetTop)
+        this.ctx.stroke()
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(e.touches[0].clientX - this.canvas.offsetLeft, e.touches[0].clientY - this.canvas.offsetTop);
+        this.ctx.beginPath()
+        this.ctx.moveTo(e.touches[0].clientX - this.canvas.offsetLeft, e.touches[0].clientY - this.canvas.offsetTop)
       },
     },
   };
-  </script>
-  
-  <style scoped>
-  canvas {
-    border: 1px solid #fff;
-  }
-
-  .color-box {
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    margin: 0 5px;
-    cursor: pointer;
-    border-radius: 50%;
-  }
-  </style>
-  
+</script>
