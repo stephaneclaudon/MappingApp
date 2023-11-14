23c0244ca5486eb3ec0b4b06fbc8b6a787513713
ValentinGassend <valentin.gassend@gmail.com>
2023-11-14 09:02:05 +0100

    git ignore licence

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SlideShowView.vue')
+    },
+    {
+      path: '/stickers',
+      name: 'stickers',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/StickersView.vue')
     }
   ]
 })
