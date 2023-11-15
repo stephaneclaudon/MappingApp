3830a4532d1cb9cc0fd72b87098b24d412f0d2d3
MathieuDubart <mathieu.dubart@icloud.com>
2023-11-15 14:29:27 +0100

    adding slider params in config

--- src/components/SwiperPreview.vue
+++ src/components/SwiperPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="slideshow-container" :style="{ backgroundImage: `${items[currentIndex].bgImg}`}">
+  <div class="slideshow-container" :style="{ backgroundImage: `${'url('+slides[currentIndex].bgImg+')'}`}">
     <swiper-container
       :effect="'coverflow'"
       :grabCursor="true"
@@ -19,23 +19,24 @@
       @swiperslidechange="onSlideChange"
       class="mySwiper swiper"
     >
-      <swiper-slide v-for="(item, index) in items" :key="index" style="padding: 2.5rem" :class="'img'+index">
-        <!-- <router-link :to="item.path" @click.native="goToVideo(item.path)"> -->
-          <a @click="goToVideo(item.path)">
-            <img :src="item.imgSrc"  class="slideshow-img" />
-          </a>
-        <!-- </router-link> -->
+      <swiper-slide v-for="(slide, index) in slides" :key="index" style="padding: 2.5rem">
+        <router-link :to="route+slide.video">
+          <img :src="slide.image"  class="slideshow-img" :style="{ boxShadow: `${slides[currentIndex].mainColor+' 0px 0px 30px 2px'}`}"/>
+        </router-link>
       </swiper-slide>   
     </swiper-container>
-    <div class="platform" :style="{ backgroundColor: `${items[currentIndex].bgColor}`}">
+
+    <div class="platform" :style="{ backgroundColor: `${slides[currentIndex].mainColor}`}">
+      <div class="elipsis" :style="{ backgroundColor: `${slides[currentIndex].mainColor}`}">
+      </div>
     </div>
     <!-- <div class="slider-shadow" style="background-color: color-mix(in srgb, , black)"></div> -->
     <div class="slider-shadow" :style="{ backgroundColor: `color-mix(in srgb, ${items[currentIndex].bgColor} 40% , rgba(0, 0, 0, 0.2))` }"></div>
     
     <div class="slider-shadow-v2" :style="{ backgroundColor: `color-mix(in srgb, ${items[currentIndex].bgColor} 40% , rgba(0, 0, 0, 0.2))` }"></div>
     <div style="position: absolute; bottom: 7rem; text-align: center;">
-      <h1 style="font-size: 4.5rem;">{{ items[currentIndex].title }}</h1>
-      <p style="font-size: 1.5rem;">helo helo helo helo helo</p>
+      <h1 style="font-size: 4.5rem;">{{ slides[currentIndex].title }}</h1>
+      <p style="font-size: 1.5rem;">{{ slides[currentIndex].author }}</p>
     </div>
   </div>
 </template>
@@ -66,73 +67,6 @@
       }
     },
     setup() {
-      const items = [
-          {
-            path: "/vid/0",
-            imgSrc: "/thumbnails/0.jpg",
-            bgColor: "rgb(100, 100, 100)",
-            bgImg: "url(/thumbnails/0.jpg)",
-            title: "N",
-            author:  ""
-          },
-          {
-            path: "/vid/1",
-            imgSrc: "/thumbnails/1.jpg",
-            bgColor: "rgb(230, 203, 255)",
-            bgImg: "url(/thumbnails/1.jpg)",
-            title: "E",
-            author: ""
-          },
-          {
-            path: "/vid/2",
-            imgSrc: "/thumbnails/2.jpg",
-            bgColor: "rgb(218, 255, 224)",
-            bgImg: "url(/thumbnails/2.jpg)",
-            title: "G",
-            author: ""
-          },
-          {
-            path: "/vid/3",
-            imgSrc: "/thumbnails/3.jpg",
-            bgColor: "rgb(254, 255, 218)",
-            bgImg: "url(/thumbnails/3.jpg)",
-            title: "R",
-            author: ""
-          },
-          {
-            path: "/vid/4",
-            imgSrc: "/thumbnails/4.jpg",
-            bgColor: "rgb(255, 207, 184)",
-            bgImg: "url(/thumbnails/4.jpg)",
-            title: "O",
-            author: ""
-          },
-          {
-            path: "/vid/5",
-            imgSrc: "/thumbnails/5.jpg",
-            bgColor: "rgb(100, 100, 100)",
-            bgImg: "url(/thumbnails/5.jpg)",
-            title: "CACA",
-            author: ""
-          },
-          {
-            path: "/vid/6",
-            imgSrc: "/thumbnails/6.jpg",
-            bgColor: "rgb(255, 218, 218)",
-            bgImg: "url(/thumbnails/6.jpg)",
-            title: "PROUT",
-            author: ""
-          },
-          {
-            path: "/vid/7",
-            imgSrc: "/thumbnails/7.jpg",
-            bgColor: "rgb(255, 240, 218)",
-            bgImg: "url(/thumbnails/7.jpg)",
-            title: "helo",
-            author: ""
-          },
-        ]
-
       const currentIndex = ref(0)
 
       const goToVideo = (path) => {
@@ -164,7 +98,6 @@
         onSlideChange,
         goToVideo,
         modules: [EffectCoverflow, Pagination],
-        items,
         currentIndex
       };
     },
@@ -180,7 +113,7 @@ html, body {
 .slideshow-container {
   width: 100vw;
   height: 100vh;
-  background-image: url("/thumbnails/1.jpg");
+  /* background-image: url("/thumbnails/1.jpg"); */
   background-repeat: no-repeat;
   background-size: cover; 
   background-position: center;
@@ -189,6 +122,7 @@ html, body {
   justify-content: center;
   align-items: center;
   background-position: center center;
+  transition: 0.7s;
 }
 .swiper {
   width: 100%;
@@ -220,8 +154,6 @@ html, body {
 }
 .slideshow-img {
   border-radius: 1rem;
-  box-shadow: rgba(255, 255, 255, 0.7) 0px 0px 35px 4px;
-  /* filter: drop-shadow(50px 10px 4px #44dd72); */
 }
 .platform {
   width: 170vw;
@@ -232,6 +164,7 @@ html, body {
   bottom: 0;
   border-top-right-radius: 50%;
   border-top-left-radius: 50%;
+  transition: 0.7s;
 }
 .slider-shadow{
   width: 100%;
@@ -245,6 +178,32 @@ html, body {
   background-color: color-mix(in srgb, rgb(230, 203, 255) 50%, rgba(0, 0, 0, 0.2));
   transform-origin: bottom center;
   filter: blur(20px)
+}
+.slider-shadow-v2 {
+  width: 100%;
+  height: 150px; 
+  bottom: -4rem;
+  left: -12rem;
+  border-radius: 100px;
+  transform: rotate(15deg);
+  position: absolute;
+  background-color: color-mix(in srgb, rgb(230, 203, 255) 50%, rgba(0, 0, 0, 0.2));
+  transform-origin: bottom center;
+  filter: blur(20px)
+}
+.slider-shadow{
+  width: 100%;
+  height: 80px;
+  bottom: 11rem;
+  left: 30rem;
+  border-radius: 100px;
+  transform: rotate(18deg);
+  position: absolute;
+  /* background-color: rgba(0, 0, 0, 0.2); */
+  background-color: color-mix(in srgb, rgb(230, 203, 255) 50%, rgba(0, 0, 0, 0.2));
+  transform-origin: bottom center;
+  filter: blur(20px);
+  transition: 0.7s;
 }
 .slider-shadow-v2 {
   width: 100%;
