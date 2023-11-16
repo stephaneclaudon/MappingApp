3c61886266725a5932d584df1f5c8cb37c7d8b0c
ptitjbus <ptitjbus02@gmail.com>
2023-11-16 16:09:25 +0100

    init new canvas

---
+++ src/components/NewWhiteBoard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="whiteboard">
+        <div class="canvas-wrapper">
+            <canvas></canvas>
+            <div class="stickers-bar">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import gsap from "gsap";
+import {Draggable} from 'gsap/Draggable';
+import config from '../../config.json';
+
+gsap.registerPlugin(Draggable)
+export default {}
+
+
+</script>
