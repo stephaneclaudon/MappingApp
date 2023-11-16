b567e0f46448b0f6ade1e8e7ac009586b3e89004
ValentinGassend <valentin.gassend@gmail.com>
2023-11-16 17:45:06 +0200

    Update Whiteboard.vue

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -579,8 +579,38 @@ export default {
           console.log("curAngle ", curAngle)
           // Update the scale and rotation of the dragged sticker
           const dragged = document.getElementsByClassName('dragged')[0];
-          console.log("dragged.style.rotation ",dragged.style.rotation)
+          var el = dragged
+          var st = window.getComputedStyle(el, null);
+          var tr = st.getPropertyValue("-webkit-transform") ||
+              st.getPropertyValue("-moz-transform") ||
+              st.getPropertyValue("-ms-transform") ||
+              st.getPropertyValue("-o-transform") ||
+              st.getPropertyValue("transform") ||
+              "FAIL";
 
+// With rotate(30deg)...
+// matrix(0.866025, 0.5, -0.5, 0.866025, 0px, 0px)
+          console.log('Matrix: ' + tr);
+
+// rotation matrix - http://en.wikipedia.org/wiki/Rotation_matrix
+
+          var values = tr.split('(')[1].split(')')[0].split(',');
+          var a = values[0];
+          var b = values[1];
+          var c = values[2];
+          var d = values[3];
+
+          var scale = Math.sqrt(a*a + b*b);
+
+          console.log('Scale: ' + scale);
+
+// arc sin, convert from radians to degrees, round
+          var sin = b/scale;
+// next line works for 30deg but not 130deg (returns 50);
+// var angle = Math.round(Math.asin(sin) * (180/Math.PI));
+          var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+
+          console.log('Rotate: ' + angle + 'deg');
           if (dragged) {
             gsap.to(dragged, {
               rotation: dragged.style.rotation + this.rotationAngle,
