625bc5741545dd7514b1c541adca32dc6379b2e5
ptitjbus <ptitjbus02@gmail.com>
2023-11-16 09:38:54 +0100

    test event listener

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -35,7 +35,7 @@
       <div class="stickers-container">
         <svg class="stickers" style=""></svg>
       </div>
-      <canvas class="canvas" ref="canvas"></canvas>
+      <canvas class="canvas" ref="canvas" id="canvas"></canvas>
     </div>
   </div>
   <a id="download"></a>
@@ -77,6 +77,8 @@ export default {
       rotateStartAngle: 0,
       rotateCurrentAngle: 0,
       isEraserSelected: false,
+      evCache: new Array(),
+      prevDiff: -1,
     };
   },
   async mounted() {
@@ -131,7 +133,85 @@ export default {
     this.updatePrevCanvas();
   },
   methods: {
+    init() {
+    // Install event handlers for the pointer target
+    var el=document.getElementById("canvas");
+    el.onpointerdown = pointerdown_handler;
+    el.onpointermove = pointermove_handler;
 
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    el.onpointerup = pointerup_handler;
+    el.onpointercancel = pointerup_handler;
+    el.onpointerout = pointerup_handler;
+    el.onpointerleave = pointerup_handler;
+    },
+    pointermove_handler(ev) {
+    // This function implements a 2-pointer horizontal pinch/zoom gesture. 
+    //
+    // If the distance between the two pointers has increased (zoom in), 
+    // the taget element's background is changed to "pink" and if the 
+    // distance is decreasing (zoom out), the color is changed to "lightblue".
+    //
+    // This function sets the target element's border to "dashed" to visually
+    // indicate the pointer's target received a move event.
+    ev.target.style.border = "dashed";
+
+    // Find this event in the cache and update its record with this event
+    for (var i = 0; i < this.evCache.length; i++) {
+      if (ev.pointerId == this.evCache[i].pointerId) {
+          this.evCache[i] = ev;
+      break;
+      }
+    }
+
+    // If two pointers are down, check for pinch gestures
+    if (this.evCache.length == 2) {
+      // Calculate the distance between the two pointers
+      var curDiff = Math.sqrt(Math.pow(this.evCache[1].clientX - this.evCache[0].clientX, 2) + Math.pow(this.evCache[1].clientY - this.evCache[0].clientY, 2));
+
+      if (this.prevDiff > 0) {
+        if (curDiff > this.prevDiff) {
+          // The distance between the two pointers has increased
+          log("Pinch moving OUT -> Zoom in", ev);
+          ev.target.style.background = "pink";
+        }
+        if (curDiff < this.prevDiff) {
+          // The distance between the two pointers has decreased
+          log("Pinch moving IN -> Zoom out",ev);
+          ev.target.style.background = "lightblue";
+        }
+      }
+
+      // Cache the distance for the next move event 
+      this.prevDiff = curDiff;
+    }
+    },
+    pointerup_handler(ev) {
+      log(ev.type, ev);
+      // Remove this pointer from the cache and reset the target's
+      // background and border
+      remove_event(ev);
+      ev.target.style.background = "white";
+      ev.target.style.border = "1px solid black";
+    
+      // If the number of pointers down is less than two then reset diff tracker
+      if (this.evCache.length < 2) this.prevDiff = -1;
+    },
+    remove_event(ev) {
+    // Remove this event from the target's cache
+    for (var i = 0; i < this.evCache.length; i++) {
+      if (this.evCache[i].pointerId == ev.pointerId) {
+        this.evCache.splice(i, 1);
+        break;
+      }
+    }
+    },
+    pointerdown_handler(ev) {
+      // The pointerdown event signals the start of a touch interaction.
+      // This event is cached to support 2-finger gestures
+      this.evCache.push(ev);
+    },
     enableEraser() {
       this.isEraserSelected = true;
     },
@@ -218,7 +298,7 @@ export default {
 
       // Utilisez le canvas des différences comme nécessaire
       // Par exemple, vous pouvez afficher le canvas des différences dans une image
-      console.log(this.diffCanvas.toDataURL());
+      // console.log(this.diffCanvas.toDataURL());
     },
 
     downloadImage(imageName) {
