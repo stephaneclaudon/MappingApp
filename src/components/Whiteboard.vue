e1bf9f6acaeb435b667bb4b64cbd6664298ec8e6
ValentinGassend <valentin.gassend@gmail.com>
2023-11-16 08:50:17 +0100

    Update Whiteboard.vue

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -464,23 +464,25 @@ export default {
         // Update the scale of the dragged sticker
         const dragged = document.querySelector(".dragged");
         if (dragged) {
+          console.log(pinchScale)
           const clampedScale = Math.min(Math.max(pinchScale, 0.5), 5);
-          gsap.to(dragged, { scale: clampedScale });
+          gsap.to(dragged, { scale: pinchScale });
         }
 
         // Update the start distance for the next pinch event
         this.pinchStartDistance = pinchEndDistance;
-      } else if (e.touches.length === 1) {
-        // Handle single touch (drag) logic here
-        // You may want to adjust the existing mobileDraw logic
-        // for dragging the sticker with one finger
-
-        // Reset the scale of the dragged sticker to 1
-        const dragged = document.querySelector(".dragged");
-        if (dragged) {
-          gsap.to(dragged, { scale: 1 });
-        }
       }
+      // else if (e.touches.length === 1) {
+      //   // Handle single touch (drag) logic here
+      //   // You may want to adjust the existing mobileDraw logic
+      //   // for dragging the sticker with one finger
+      //
+      //   // Reset the scale of the dragged sticker to 1
+      //   const dragged = document.querySelector(".dragged");
+      //   if (dragged) {
+      //     gsap.to(dragged, { scale: 1 });
+      //   }
+      // }
     },
     initializeMap() {
       this.map = localforage.createInstance({
